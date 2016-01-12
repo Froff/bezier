eff65c557f7f5bfabe138e202283ad103b6c8cce
Halvor <stektpotet@gmail.com>
2016-01-12 23:57:35 +0100

    minor changes

--- Assets/BezierSpline.cs
+++ Assets/BezierSpline.cs
@@ -8,15 +8,20 @@ public class BezierSpline : MonoBehaviour
 
     public BezierCurve[] curves = { new BezierCurve(Vector3.right, Vector3.up, Vector3.forward) };
 
-    //public Vector3 Position(float t)
-    //{
-    //    float a = 1 - t;
+    public Vector3 Position(float t)
+    {
 
-    //    return transform.TransformPoint(
-    //        3 * Mathf.Pow(a, 2) * t * CtrlPoints[0] +
-    //        3 * a * Mathf.Pow(t, 2) * CtrlPoints[1] +
-    //        Mathf.Pow(t, 3) * CtrlPoints[2]);
-    //}
+        t = (Mathf.Clamp01(t) * (curves.Length));
+        int i = (int)t;     
+        Debug.Log(i + " , " + t);
+        t -= i;
+        
+        if (i==0)
+        {
+            return curves[i].Position(t, transform.position);
+        }
+        return curves[i].Position(t, curves[i - 1].c);
+    }
 }
 
 [System.Serializable]
@@ -49,10 +54,10 @@ public class BezierCurve
     public static Vector3 Position(float t, Vector3 o, Vector3 a, Vector3 b, Vector3 c)
     {
         float n = 1 - t;
-        Vector3 p = o * n * n * n;  //(1-t)^3*a
-        p += 3 * n * n * t * a;     //3(1-t)^2*t*b
-        p += 3 * n * t * t * b;     //3(1-t)*t^2*c
-        p += t * t * t * c;         //t^3*d
+        Vector3 p = o * n * n * n; 
+        p += 3 * n * n * t * a;    
+        p += 3 * n * t * t * b;     
+        p += t * t * t * c;         
 
         return p;
     }
--- Assets/Editor/BezierEditor.cs
+++ Assets/Editor/BezierEditor.cs
@@ -161,67 +161,14 @@ public class BezierEditor : Editor
         Handles.color = Color.green;
         Vector3 v = BezierCurve.Position(0.0f, o, a, b, c);
 
-        for (int i = 1; i <= 50; i++)
+        for (int i = 1; i <= 30; i++)
         {
-            Vector3 u = BezierCurve.Position((float)i/50.0f, o, a, b, c);
+            Vector3 u = BezierCurve.Position((float)i/30.0f, o, a, b, c);
+            
             Handles.DrawLine(v, u);
             v = u;
         }
     }
-
-    
-
-    //    for (int i = 0; i < bezier.CtrlPoints.Length; i+=3)
-    //    {
-    //        //DEFINE POINTS in localspace
-
-    //        Vector3 b = handleTransform.TransformPoint(bezier.CtrlPoints[i]);
-    //        Vector3 c = handleTransform.TransformPoint(bezier.CtrlPoints[i + 1]);
-    //        Vector3 d = handleTransform.TransformPoint(bezier.CtrlPoints[i + 2]);
-
-    //        Vector3[] curvePoints = { a, b, c, d }; //These have been transformed into localspace points
-
-    //        BezierEditor.DrawCurve(bezier);
-
-    //        DrawControlPointLine(a, b, Color.red);
-    //        DrawControlPointLine(d, c, Color.blue);
-
-    //        Handles.color = Color.white;
-    //        for(int l = 0; l < 4; l++)
-    //        {
-    //            if (Handles.Button(curvePoints[l], handleRotation, handleSize * HandleUtility.GetHandleSize(curvePoints[l]), pickSize * HandleUtility.GetHandleSize(curvePoints[l]), Handles.SphereCap))
-    //            {
-    //                selectedIndex = (selectedIndex != (i-1) + l) ? (i - 1) + l : -1;
-    //            }
-    //            if (selectedIndex == (i - 1) + l)
-    //            {
-    //                EditorGUI.BeginChangeCheck();
-    //                curvePoints[l] = Handles.DoPositionHandle(curvePoints[l], handleRotation);
-    //                if (EditorGUI.EndChangeCheck())
-    //                {
-    //                    Undo.RecordObject(bezier, "Move Bezier Point");
-    //                    EditorUtility.SetDirty(bezier);
-    //                    bezier.CtrlPoints[(i - 1) + l] = handleTransform.InverseTransformPoint(curvePoints[l]);
-    //                }
-    //            }
-    //        }
-
-
-    //    }
-
-    //}
-
-    //static void DrawCurve(BezierSpline spline)
-    //{
-    //    Handles.color = Color.green;
-    //    Vector3 v = spline.Position(0f);
-    //    for (int i = 1; i <= 50*((spline.CtrlPoints.Length-1)/3); i++)
-    //    {
-    //        Vector3 u = spline.Position((float)i / 50 * ((spline.CtrlPoints.Length - 1) / 3));
-    //        Handles.DrawLine(v, u);
-    //        v = u;
-    //    }
-    //}
 
     static void DrawHelpLine(Vector3 a, Vector3 b, Color col)
     { 
